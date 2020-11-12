a824022495e41752649c5222088ea9c97939778d
Reem <ranalsolami@gmail.com>
2020-11-12 18:13:18 +0300

    editneeded

--- UGMsystem/src/ugmsystem/UGMsystem.java
+++ UGMsystem/src/ugmsystem/UGMsystem.java
@@ -10,6 +10,9 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import static ugmsystem.User.accounts;
 
 /**
@@ -23,18 +26,26 @@ public class UGMsystem {
      */
     public static void main(String[] args) throws IOException {
         
-      BufferedReader read2 = new BufferedReader(new FileReader(accounts));
+     /* BufferedReader read2 = new BufferedReader(new FileReader(accounts));
        FileWriter fileWriter = new FileWriter(accounts, false);
       BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); 
       
       String line ;
       while((line=read2.readLine())!=null){
           bufferedWriter.write(" "); 
-      } 
+      } */
           
-
-    
-    
+     User r = new User("12345", "reem","moh","0567899879","g@h.com");
+     
+        r.register();
+        System.out.println("User pass before"+ r.getPassword());
+       // r.setPassword("1");
+        
+        r.setfName("rrrr");
+       // r.setPhoneNumber("0786");
+       // r.setEmail("reem@gmail.com");
+     
+       System.out.println("User pass after"+ r.getPassword());
     }
     
 }
--- UGMsystem/src/ugmsystem/User.java
+++ UGMsystem/src/ugmsystem/User.java
@@ -10,6 +10,7 @@ package ugmsystem;
  * @author reemalsolami
  */
 import java.io.*; 
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,6 +24,7 @@ public class User {
     private int guestID=111;
     static String userAccount [];
     public static File accounts= new File ("account.txt");
+    private ArrayList userInfo = new ArrayList();
  
    
     
@@ -83,29 +85,29 @@ public class User {
     }
 
     public void setPassword(String password) {
-        setMatch(password);
+        updatePassword(password);
         this.password = password;
         
         
     }
 
     public void setfName(String fName) {
-        setMatch(fName);
+        updateFName(fName);
         this.fName = fName;
     }
 
     public void setlName(String lName) {
-        setMatch(lName);
+        updateLName(lName);
         this.lName = lName;
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        setMatch(phoneNumber);
+        updatePhonenumber(phoneNumber);
         this.phoneNumber = phoneNumber;
     }
 
     public void setEmail(String email) {
-        setMatch(email);
+        updateEmail(email);
         this.email = email;
     }
     
@@ -120,13 +122,27 @@ public class User {
          
           if(noMatch())  {
         try {
-            
+            BufferedReader read2 = new BufferedReader(new FileReader(accounts));
+            String buffer [];
             FileWriter fileWriter = new FileWriter(accounts, true);;
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            
             bufferedWriter.write(this.id +","+this.password+","+
                     this.fName+","+this.lName+","+this.getPhoneNumber()+","+ this.getEmail());
+           
             bufferedWriter.write("\n");
+          
             bufferedWriter.flush();
+            
+            
+            buffer  = read2.readLine().split(",");
+            System.out.println("buffer[1]"+buffer[1]);
+            userInfo.add(Arrays.asList(buffer));
+            
+          
+            
+            
+            
           
         } catch (FileNotFoundException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
@@ -193,10 +209,10 @@ public class User {
           String line;
         try {
             BufferedReader read2 = new BufferedReader(new FileReader(accounts));
-        
+            
        
           while((line=read2.readLine()) != null) {
-              System.out.println("here");
+            
               String s [] = line.split(",");
              if(s[4].equalsIgnoreCase(this.phoneNumber.trim()) || s[5].equalsIgnoreCase(this.email.trim())) {
                  return false;
@@ -210,53 +226,63 @@ public class User {
         return true;}
       
       
-        public void setMatch(String match){
-          String line;
-          String [] setter;
-        try {
-            BufferedReader read2 = new BufferedReader(new FileReader(accounts));
- 
       
-          while((line=read2.readLine()) != null) {
-              
-              if(line.contains(match.trim())) {
-                 setter=line.split(",");
-                if(match.trim().equals(this.password)){
-                    setter[1]=match;
-                }
-                else if(match.trim().equals(this.fName)){
+      
+        public void updatePassword(String match){
+        
+          String [] setter;
+           try {
+           
+              for(int i =0 ; i<userInfo.size();i++){
+                 setter= userInfo.get(i).toString().split("[\\[,\\]]");
+                
+                if(setter[2].trim().equals(this.password)){
+                    System.out.println("line 239 "+setter[2].trim().equals(this.password));
                     setter[2]=match;
-                }
-                else if(match.trim().equals(this.lName)){
-                    setter[3]=match;
-                }
-                else if(match.trim().equals(this.phoneNumber)){
-                    setter[4]=match;
-                }
-                else {
-                    setter[5]=match;
-                }
+                  }
                 
                  FileWriter fileWriter = new FileWriter(accounts, false);
                  BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
                 
-                 bufferedWriter.write(setter[0]+","+setter[1]+","+
-                         setter[2]+","+setter[3]+","+setter[4]+","+setter[5]);
+                 bufferedWriter.write(setter[1]+","+setter[2]+","+
+                         setter[3]+","+setter[4]+","+setter[5]+","+setter[6]);
                  
+                 bufferedWriter.write("\n");
                  bufferedWriter.close();
               
              
+
           }
-              else{
-                   FileWriter fileWriter = new FileWriter(accounts, true);
-                   BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                   bufferedWriter.write(line);
-                   bufferedWriter.write("\n");
-                   bufferedWriter.close();
-                   
-                   
-                   
-            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (IOException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+         
+         public void updateFName(String match){
+        
+          String [] setter;
+           try {
+           
+              for(int i =0 ; i<userInfo.size();i++){
+                 setter= userInfo.get(i).toString().split("[\\[,\\]]");
+             if(setter[3].trim().equals(this.fName)){
+                    setter[3]=match;
+                }
+                
+                
+                 FileWriter fileWriter = new FileWriter(accounts, false);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                
+                 bufferedWriter.write(setter[1]+","+setter[2]+","+
+                         setter[3]+","+setter[4]+","+setter[5]+","+setter[6]);
+                 
+                 bufferedWriter.write("\n");
+                 bufferedWriter.close();
+              
+             
+
           }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
@@ -266,4 +292,104 @@ public class User {
     }
       
      
+
+         public void updateLName(String match){
+        
+          String [] setter;
+           try {
+           
+              for(int i =0 ; i<userInfo.size();i++){
+                 setter= userInfo.get(i).toString().split("[\\[,\\]]");
+             if(setter[4].trim().equals(this.lName)){
+                    setter[4]=match;
+                }
+                
+                
+                 FileWriter fileWriter = new FileWriter(accounts, false);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                
+                 bufferedWriter.write(setter[1]+","+setter[2]+","+
+                         setter[3]+","+setter[4]+","+setter[5]+","+setter[6]);
+                 
+                 bufferedWriter.write("\n");
+                 bufferedWriter.close();
+              
+             
+
+          }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (IOException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+         
+           public void updatePhonenumber(String match){
+        
+              String [] setter;
+              try {
+           
+              for(int i =0 ; i<userInfo.size();i++){
+                 setter= userInfo.get(i).toString().split("[\\[,\\]]");
+                 if(setter[5].trim().equals(this.phoneNumber)){
+                    setter[5]=match;
+                     }
+                
+                
+                 FileWriter fileWriter = new FileWriter(accounts, false);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                
+                 bufferedWriter.write(setter[1]+","+setter[2]+","+
+                         setter[3]+","+setter[4]+","+setter[5]+","+setter[6]);
+                 
+                 bufferedWriter.write("\n");
+                 bufferedWriter.close();
+              
+             
+
+          }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (IOException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    
+      
+     
+}         public void updateEmail(String match){
+        
+          String [] setter;
+           try {
+           
+              for(int i =0 ; i<userInfo.size();i++){
+                 setter= userInfo.get(i).toString().split("[\\[,\\]]");
+             if(setter[6].trim().equals(this.email)){
+                    setter[6]=match;
+                }
+                
+                
+                 FileWriter fileWriter = new FileWriter(accounts, false);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                
+                 bufferedWriter.write(setter[1]+","+setter[2]+","+
+                         setter[3]+","+setter[4]+","+setter[5]+","+setter[6]);
+                 
+                 bufferedWriter.write("\n");
+                 bufferedWriter.close();
+              
+             
+
+          }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (IOException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    
+      
+     
 }
+}
+      
+     
+

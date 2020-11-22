bc9d7697a5de7af9111bc9825d7954f0e10c5b80
99imoon <99imoon@users.noreply.github.com>
2020-11-22 22:01:08 +0300

    DONE REEM TESTING

---
+++ UGMsystem/test/ugmsystem/BuyerTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ugmsystem;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jae
+ */
+public class BuyerTest {
+
+    /**
+     * Test of findUser method, of class Buyer.
+     */
+    @Test
+    public void testFindUser() throws Exception {
+        String userId = "33536996";
+        Buyer expResult = new Buyer ("33536996","D1234567","Anas","Khaled","0566666667","anas@gmail.com");
+        Buyer result = Buyer.findUser(userId);
+        String expbuyerArray []={expResult.getId(), expResult.getPassword(), expResult.getfName(), expResult.getlName(), expResult.getPhoneNumber(), expResult.getEmail()};
+        String resultbuyerArray []= {result.getId(), result.getPassword(), result.getfName(), result.getlName(), result.getPhoneNumber(), result.getEmail()};
+        
+        assertArrayEquals(expbuyerArray, resultbuyerArray);
+    }
+    
+}
---
+++ UGMsystem/test/ugmsystem/UserTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ugmsystem;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jae
+ */
+public class UserTest {
+
+    /**
+     * Test of login method, of class User.
+     */
+    @Test
+    public void testLogin() throws Exception {
+        System.out.println("login");
+        String id = "33536666";
+        String password = "A1234567";
+        boolean expResult = true;
+        boolean result = User.login(id, password);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of register method, of class User.
+     */
+    @Test
+    public void testRegister() throws Exception {
+        User instance = new User("M1234567","Samaher","Alghamdi","0567321459","samaher123@gmail.com");
+        int expResult = 1;
+        int result = instance.register();
+    
+        if(result==1)
+        {
+          assertEquals(expResult, result);
+        }
+        else
+        {
+            System.out.println("User Account With Same Information Already Exists! Please Specify New User Information.");
+            assertTrue(result==-1);
+        }
+    }
+
+    
+}
